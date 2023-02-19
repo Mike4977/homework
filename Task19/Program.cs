9afd5fc0ea162534318dee42d0bd130be3c73fe5
mike77 <weilliss@mail.ru>
2023-02-19 14:42:25 +0600

    add 'for' in task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,6 +7,7 @@
 // 23432 -> да
 
 
+// Любое число с "for"
 
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -19,16 +20,40 @@ else Console.Write("Нет");
 int Check (int num)
 {
     int reversNum = 0;
-    while (num >= 1) 
+    for (int i = num; i >= 1; i = i / 10) 
     {
-        reversNum = reversNum * 10 + num % 10;
-        num = num / 10;
+        reversNum = reversNum * 10 + i % 10;
     }
     return reversNum; 
 }
 
 
 
+// Любое число c "while"
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int checkNumber = Check(number);
+// // Console.WriteLine(checkNumber);
+
+// if (checkNumber == number) Console.Write("Да");
+// else Console.Write("Нет");
+
+// int Check (int num)
+// {
+//     int reversNum = 0;
+//     while (num >= 1) 
+//     {
+//         reversNum = reversNum * 10 + num % 10;
+//         num = num / 10;
+//     }
+//     return reversNum; 
+// }
+
+
+
+
+// Пятизначное число
 
 // Console.WriteLine("Введите пятизначное число");
 // int number = Convert.ToInt32(Console.ReadLine());
