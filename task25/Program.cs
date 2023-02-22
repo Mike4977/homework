75ec5421ad6096a92596916ea27bcb5f280d0999
mike77 <weilliss@mail.ru>
2023-02-22 18:10:35 +0600

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int a = Promt("Введите первое число");
+int b = Promt("Введите второе число");
+
+int exponention = Exponention(a, b);
+
+Console.WriteLine($"Число {a} в степени {b} = {exponention}");
+
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    string value = (Console.ReadLine());
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int Exponention(int numA, int numB)
+{
+	int exp = 1;
+	for (int i = 1; i <= numB; i++)
+	{
+		exp = exp * numA;
+	}
+	return exp;
+}
