8f9a1829b1253be6a5c8143c772247b346566ad1
mike77 <weilliss@mail.ru>
2023-03-13 17:44:14 +0600

    optimization

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -15,10 +15,7 @@ Console.WriteLine($"m = {numberM}, n = {numberN} -> A(m, n) = {Ackerman(numberM,
 
 int Ackerman(int m, int n)
 {
-    if(m == 0) return n + 1;
-    else
-    {
-        if(m != 0 && n == 0) return Ackerman(m - 1, 1);
-        else return Ackerman(m - 1, Ackerman(m, n - 1));
-    }
+    if(m == 0) return n + 1; 
+    if(m != 0 && n == 0) return Ackerman(m - 1, 1);
+    return Ackerman(m - 1, Ackerman(m, n - 1));
 }
